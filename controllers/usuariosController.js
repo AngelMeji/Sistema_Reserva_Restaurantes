24839d742bfdb40d876fcfd0b2c408713257a31c
AngelMeji <joseangelmejiamedina@gmail.com>
2025-12-11 10:24:27 -0500

    Agregamos funcionalidad y vista de prueba de olvide contraseña

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -2,6 +2,7 @@ import { check, validationResult } from "express-validator";
 import bcrypt from "bcrypt";
 import { generarId, generarJWT } from "../helpers/tokens.js";
 import Usuario from "../models/Usuarios.js";
+import { emailRegistro, emailOlvidePassword } from "../helpers/emails.js";
 
 const formularioLogin = (req, res) => {
   res.render("auth/login", {
@@ -81,6 +82,63 @@ const autenticar = async (req, res) => {
       // sameSite: true
     })
     .redirect(redirectUrl);
+};
+
+const formularioOlvidePassword = (req, res) => {
+  res.render("auth/olvide-password", {
+    tituloPagina: "Olvide Contraseña",
+    csrfToken: req.csrfToken(),
+  });
+};
+
+const resetPassword = async (req, res) => {
+  // Validaciones
+  await check("email")
+    .isEmail()
+    .withMessage("Esto no parece un correo")
+    .run(req);
+
+  let resultado = validationResult(req);
+
+  // Verificar que el resultado este vacio
+  if (!resultado.isEmpty()) {
+    // Errores
+    return res.render("auth/olvide-password", {
+      tituloPagina: "Olvide contraseña",
+      errores: resultado.array(),
+      csrfToken: req.csrfToken(),
+    });
+  }
+
+  // Buscar el usuario
+
+  const { email } = req.body;
+
+  const usuario = await Usuario.findOne({ where: { email } });
+  if (!usuario) {
+    return res.render("auth/olvide-password", {
+      tituloPagina: "Recuperar contraseña",
+      csrfToken: req.csrfToken(),
+      errores: [{ msg: "El email no existe" }],
+    });
+  }
+
+  // Generar un token y enviar un email
+  usuario.token = generarId();
+  await usuario.save();
+
+  // Enviar el correo
+  emailOlvidePassword({
+    nombre: usuario.nombre,
+    email: usuario.email,
+    token: usuario.token,
+  });
+
+  //Mostrar el mensaje
+  res.render("templates/mensaje", {
+    tituloPagina: "Restablece la contraseña",
+    mensaje: "Hemos enviado un correo de restablecer la contraseña",
+  });
 };
 
 const formularioRegistro = (req, res) => {
@@ -186,6 +244,72 @@ const registrar = async (req, res) => {
   }
 };
 
+const comprobarToken = async (req, res) => {
+  const { token } = req.params;
+
+  // Validar el token sea verdadero
+  const usuario = await Usuario.findOne({ where: { token } });
+
+  if (!usuario) {
+    return res.render("auth/confirmar-cuenta", {
+      tituloPagina: "Restablece tu contraseña",
+      mensaje: "Hubo un error al validar el token",
+      error: true,
+    });
+  }
+
+  // Mostrar formulario para validar la contraseña
+  res.render("auth/reset-password", {
+    tituloPagina: "Escribe tu nueva contraseña",
+    csrfToken: req.csrfToken(),
+  });
+};
+
+const nuevoPassword = async (req, res) => {
+  //Validar contraseñas
+  await check("password")
+    .isLength({ min: 6 })
+    .withMessage("La contraseña debe ser al menos de 6 caracteres")
+    .run(req);
+
+  await check("repeat_password")
+    .equals(req.body.password)
+    .withMessage("La contraseña no es igual")
+    .run(req);
+
+  let resultado = validationResult(req);
+
+  // Validar que resultado este vacio
+  if (!resultado.isEmpty()) {
+    // Errores
+    return res.render("auth/reset-password", {
+      tituloPagina: "Restablece Contraseña",
+      csrfToken: req.csrfToken(),
+      errores: resultado.array(),
+    });
+  }
+
+  const { token } = req.params;
+  const { password } = req.body;
+
+  // Identificar el usuario para hacer el cambio
+  const usuario = await Usuario.findOne({ where: { token } });
+
+  // Hashear el password
+  const salt = await bcrypt.genSalt(10);
+  usuario.password = await bcrypt.hash(password, salt);
+  usuario.token = null;
+
+  // Guardar en la DB
+  await usuario.save();
+
+  res.render("auth/confirmar-cuenta", {
+    tituloPagina: "Contraseña cambiada",
+    csrfToken: req.csrfToken(),
+    mensaje: "La contraseña se cambio correctamente",
+  });
+};
+
 const cerrarSesion = async (req, res) => {
   // Limpiar el token de la base de datos si hay un usuario autenticado
   if (req.usuario && req.usuario.id) {
@@ -200,4 +324,8 @@ export {
   formularioRegistro,
   registrar,
   cerrarSesion,
+  formularioOlvidePassword,
+  resetPassword,
+  comprobarToken,
+  nuevoPassword,
 }
--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -3,7 +3,11 @@ import {
     formularioLogin,
     autenticar,
     formularioRegistro,
+    formularioOlvidePassword,
+    resetPassword,
     registrar,
+    comprobarToken,
+    nuevoPassword,
     cerrarSesion,
 } from "../controllers/usuariosController.js";
 
@@ -16,6 +20,14 @@ router.post("/login", autenticar);
 // ## Registro
 router.get("/registro", formularioRegistro);
 router.post("/registro", registrar);
+
+// ## Olvidar Contraseña
+router.get("/olvide-password", formularioOlvidePassword);
+router.post("/olvide-password", resetPassword);
+
+// Validacion de Contraseñas (Olvide Password)
+router.get("/olvide-password/:token", comprobarToken);
+router.post("/olvide-password/:token", nuevoPassword);
 
 // ## Cerrar Sesión
 router.get("/cerrar-sesion", cerrarSesion);
