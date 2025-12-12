00db6819bb18c49a09b83ac00572ce7c8408a92e
AngelMeji <joseangelmejiamedina@gmail.com>
2025-12-12 10:24:12 -0500

    cuentas para admin y recepcionista

---
+++ seed/seeder.js
@@ -0,0 +1,51 @@
+import { exit } from "node:process";
+import usuario from "./usuario.js";
+import { Usuario } from "../models/index.js";
+import db from "../config/db.js";
+
+const importarDatos = async () => {
+  try {
+    // Autenticar
+    await db.authenticate();
+
+    //Generar las columnas
+    await db.sync();
+
+    //Insertar los datos
+    await Promise.all([
+      Usuario.bulkCreate(usuario),
+    ]);
+
+    console.log("Datos importados correctamente");
+    exit();
+  } catch (error) {
+    console.log(error);
+    exit(1);
+  }
+};
+
+const eliminarDatos = async () => {
+  try {
+    //Opcion 1
+    // await Promise.all([
+    //   Categorias.destroy({ where: {} }),
+    //   Precios.destroy({ where: {} }),
+    // ]);
+
+    //Opcion Recomendada
+    await db.sync({ force: true });
+    console.log("Eliminado correctamente!");
+    exit();
+  } catch (error) {
+    console.log(error);
+    exit(1);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importarDatos();
+}
+
+if (process.argv[2] === "-e") {
+  eliminarDatos();
+}
